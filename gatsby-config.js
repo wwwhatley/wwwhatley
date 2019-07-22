eccef7f76a1c304e85275315911605ff52451d1e
William Whatley <william@catatek.com>
2019-07-22 12:15:40 -0500

    changed name.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `William Whatley`,
+        short_name: ``,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
